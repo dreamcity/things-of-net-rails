47f098664bedc7de457baef6eb50840bf74b7829
jerusalemdax <jerusalemdax@gmail.com>
2013-09-13 11:27:34 +0800

    init with command rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ThingsOfNetRails::Application.config.secret_key_base = 'e57cd8648056748bdb05e84c909057cc61ae8f680e88dddcbc6e0171542f5e69a4f2496caec82cd1e0042614adde3a2521aa0389891e4e26fe4791c0c8f1b8c9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ThingsOfNetRails::Application.config.session_store :cookie_store, key: '_things-of-net-rails_session'
